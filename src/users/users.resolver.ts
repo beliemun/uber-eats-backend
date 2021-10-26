9f7012b120644d53b2428c85aa23476309c8778e
beliemun <beliemun@paran.com>
2021-10-26 12:42:13 +0900

    #4.9 Log In part Two

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,3 +1,4 @@
+import { ConsoleLogger } from '@nestjs/common';
 import { Args, Mutation, Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
 import { create } from 'domain';
@@ -5,35 +6,41 @@ import {
   CreateAccountInput,
   CreateAccountOutput,
 } from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { UsersService } from './users.service';
 
 @Resolver((of) => UsersResolver)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
   @Query((returns) => Boolean)
   hi() {
     return true;
   }
+
   @Mutation((returns) => CreateAccountOutput)
   async createAccount(
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const error = await this.usersService.createAccount(createAccountInput);
-      if (error) {
-        return {
-          ok: false,
-          error,
-        };
-      }
-      return {
-        ok: true,
-      };
-    } catch (e) {
-      console.log(e);
+      return this.usersService.createAccount(createAccountInput);
+    } catch (error) {
+      console.log(error);
       return {
         ok: false,
-        error: e,
+        error,
+      };
+    }
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
       };
     }
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -10,21 +11,53 @@ export class UsersService {
     @InjectRepository(User) private readonly repository: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
-      const exists = await this.repository.findOne({ email });
-      if (exists) {
-        //make error
-        return 'There is an user with that email already.';
+      const user = await this.repository.findOne({ email });
+      if (user) {
+        return {
+          ok: false,
+          error: 'There is an user with that email already.',
+        };
       }
       await this.repository.save(
         this.repository.create({ email, password, role }),
       );
+      return { ok: true };
     } catch (e) {
-      //make error
-      return 'Could not create an account.';
+      return { ok: false, error: 'Could not create an account.' };
     }
+  }
 
-    // create user & hash the password
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      const user = await this.repository.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found.',
+        };
+      }
+      const isCorrect = await user.checkPassword(password);
+      if (!isCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password.',
+        };
+      }
+      return {
+        ok: true,
+        token: 'test token',
+      };
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
