7ee1872e2adf51b4cd7c71dfde2eade311b63e47
beliemun <beliemun@paran.com>
2021-11-05 15:52:25 +0900

    #9.3 Testing createAccount part Two (05:08)

--- dist/app.module.js
+++ dist/app.module.js
@@ -36,7 +36,7 @@ AppModule = __decorate([
                 envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
                 ignoreEnvFile: process.env.NODE_ENV === 'production',
                 validationSchema: Joi.object({
-                    NODE_ENV: Joi.string().valid('dev', 'production').required(),
+                    NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
                     DB_HOST: Joi.string().required(),
                     DB_PORT: Joi.string().required(),
                     DB_USERNAME: Joi.string().required(),
@@ -56,7 +56,8 @@ AppModule = __decorate([
                 password: process.env.DB_PASSSWORD,
                 database: process.env.DB_NAME,
                 synchronize: process.env.NODE_ENV !== 'production',
-                logging: process.env.NODE_ENV !== 'production',
+                logging: process.env.NODE_ENV !== 'production' &&
+                    process.env.NODE_ENV !== 'test',
                 entities: [user_entity_1.User, verification_entity_1.Verification],
             }),
             graphql_1.GraphQLModule.forRoot({
--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+import { O_TRUNC } from 'constants';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -16,13 +26,64 @@ describe('UserModule (e2e)', () => {
   });
 
   afterAll(async () => {
+    // 테스트가 모두 끝나면 DB를 삭제
+    await getConnection().dropDatabase();
+    // Jest가 종료되기 전에 Test가 종료되지 않도록 처리
     app.close();
   });
 
-  it.todo('me');
-  it.todo('user');
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'test@google.com';
+    it('should create an account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password:"1234",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password:"1234",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
+  it.todo('userProfile');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
